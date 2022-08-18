fab75003e31c3ee27cb5a1cc3b8d60501f96713f
Kevin Kelley <kkelley622@gmail.com>
2022-08-17 19:28:08 -0500

    Displays shows under Shows

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code here
+
+document.addEventListener("DOMContentLoaded", () => {
+    listShows();
+})
+
+// list the shows under "shows"
+
+const showsList = document.getElementById("list")
+
+function listShows() {
+    fetch("http://localhost:3000/shows")
+    .then(response => response.json())
+    .then(shows => {
+        shows.forEach(show => {
+            let li = document.createElement("li");
+            li.innerText = show.title;
+            showsList.appendChild(li)
+        })})
+
+}
+
+// be able to search through the shows
+
+
+
